8d7cd78fe9db15f243ea6e8a8eb232765a1a82d3
Ajaybharath <ajaybharath009@gmail.com>
2021-11-24 15:06:15 +0530

    Updated to send mail and display the mail message parallelly

--- Emailapp/Form1.Designer.cs
+++ Emailapp/Form1.Designer.cs
@@ -29,71 +29,140 @@ namespace Emailapp
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(48, 59);
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(477, 40);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 23);
+            this.button1.Size = new System.Drawing.Size(217, 43);
             this.button1.TabIndex = 0;
             this.button1.Text = "Upload Excel";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(585, 59);
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(477, 113);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.Size = new System.Drawing.Size(217, 43);
             this.button2.TabIndex = 1;
             this.button2.Text = "Send Mail";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseCompatibleTextRendering = true;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(209, 64);
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(582, 86);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 3;
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(48, 227);
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.progressBar1.Location = new System.Drawing.Point(12, 370);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(689, 23);
+            this.progressBar1.Size = new System.Drawing.Size(459, 46);
             this.progressBar1.Step = 1;
             this.progressBar1.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 85);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 69);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.Size = new System.Drawing.Size(0, 16);
             this.label2.TabIndex = 5;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.richTextBox1.Location = new System.Drawing.Point(15, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(456, 302);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(477, 238);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(217, 178);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(191, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Sent Mails";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(712, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "i-mail";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,6 +175,9 @@ namespace Emailapp
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Emailapp/Form1.cs
+++ Emailapp/Form1.cs
@@ -6,6 +6,8 @@ using System.Net;
 using System.Net.Mail;
 using System.Threading;
 using System.Threading.Tasks;
+using System.IO;
+using System.Drawing.Drawing2D;
 
 namespace Emailapp
 {
@@ -28,23 +30,19 @@ namespace Emailapp
             if (result == DialogResult.OK) // Check if Result == "OK".
             {
                 label1.Text = "";
-                file = openFileDialog1.FileName; //get the filename with the location of the file
+                richTextBox1.Text = string.Empty;
+                file = openFileDialog1.FileName;
+                string filename = Path.GetFileName(file);
+                //get the filename with the location of the file
                 try
                 {
-                    //Create Object for Microsoft.Office.Interop.Excel that will be use to read excel file
-
+                    //Create Object for Microsoft.Office.Interop.Excel that will be use to read excel 
                     Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
-
                     Microsoft.Office.Interop.Excel.Workbook excelWorkbook = excelApp.Workbooks.Open(file);
-
                     Microsoft.Office.Interop.Excel._Worksheet excelWorksheet = excelWorkbook.Sheets[1];
-
                     Microsoft.Office.Interop.Excel.Range excelRange = excelWorksheet.UsedRange;
-
                     int rowCount = excelRange.Rows.Count; //get row count of excel data
-
                     int colCount = excelRange.Columns.Count; // get column count of excel data
-
                     //Get the first Column of excel file which is the Column Name
                     dt.Clear();
                     for (i = 1; i <= rowCount; i++)
@@ -76,10 +74,10 @@ namespace Emailapp
                         }
                         dt.Rows.Add(row); //add row to DataTable
                     }
-                    for (i = 0; i < dt.Rows.Count; i++)
-                    {  
-                        dt.Rows[i]["Time"] = DateTime.FromOADate(Convert.ToDouble(dt.Rows[i]["Time"].ToString())).ToString("h\\:mm");
-                    }
+                    //for (i = 0; i < dt.Rows.Count; i++)
+                    //{  
+                    //    dt.Rows[i]["Time"] = DateTime.FromOADate(Convert.ToDouble(dt.Rows[i]["Time"].ToString())).ToString("h\\:mm");
+                    //}
                     //close and clean excel process
                     GC.Collect();
                     GC.WaitForPendingFinalizers();
@@ -90,7 +88,8 @@ namespace Emailapp
                     Marshal.ReleaseComObject(excelWorkbook);
                     excelApp.Quit();
                     Marshal.ReleaseComObject(excelApp);
-                    label1.Text = "Excel File Uploaded";
+                    //label1.Text = "Excel File Uploaded";
+                    label1.Text = filename;
                 }
                 catch (Exception ex)
                 {
@@ -100,73 +99,104 @@ namespace Emailapp
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            button1.Enabled = false;
-            button2.Enabled = false;
-            if (label1.Text != "")
+            try
             {
-                DialogResult dr = MessageBox.Show("Do you really want to send Mails", "Verification", MessageBoxButtons.YesNo);
-                if (dr == DialogResult.Yes)
+                button1.Enabled = false;
+                button2.Enabled = false;
+                if (label1.Text != "")
                 {
-                    progressBar1.Minimum = 0;
-                    progressBar1.Maximum = dt.Rows.Count;
-                    MailMessage mailMsg = new MailMessage();
-                    mailMsg.From = new MailAddress("ajaybharath009@gmail.com", "IB IoT");
-                    for (int i = 0; i < dt.Rows.Count; i++)
+                    DialogResult dr = MessageBox.Show("Do you really want to send Mails", "Verification", MessageBoxButtons.YesNo);
+                    if (dr == DialogResult.Yes)
                     {
-                        //Task.Delay(1000);
-                        Thread.Sleep(1000);
-                        mailMsg.To.Add(new MailAddress(dt.Rows[i]["Emailid"].ToString()));
+                        progressBar1.Visible = true;
+                        progressBar1.Minimum = 0;
+                        progressBar1.Maximum = dt.Rows.Count;
+                        richTextBox1.Text = string.Empty;
+                        MailMessage mailMsg = new MailMessage();
+                        mailMsg.From = new MailAddress("ajaybharath009@gmail.com", "IB IoT");
                         mailMsg.Subject = "Reg: Ideabytes Interview Call";
-                        mailMsg.IsBodyHtml = true;
-                        mailMsg.Body = $"Dear {dt.Rows[i]["Name"]},<br/><br/>You are shortlisted for {dt.Rows[i]["Position"]}, as we discussed your interview was scheduled on {dt.Rows[i]["Date"]} at {dt.Rows[i]["Time"]}.<br/><br/>" + @"<img src='https://adminiot.dgtrak.online/FTP_Sensorcnt/domain/IBThanks.png'/>"
-                        + "<br/><br/> With Regards <br/> <hr/>HR Manager<br/>Ideabytes Inc<br/>Website: www.ideabytes.com<br/><br/>" + "<img src='https://adminiot.dgtrak.online/FTP_Sensorcnt/domain/IBMailImage.png'/>"
-                        + "<br/><br/>Important: This email and any files transmitted with it are confidential and intended solely for the use of the individual or entity to whom they are addressed. If you have received this email in error please notify the system manager. " +
-                        "Please notify the sender immediately by e-mail if you have received this e-mail by mistake and delete this e-mail from your system. If you are not the intended recipient you are notified that disclosing, copying, distributing or taking any action in " +
-                        "reliance on the contents of this information is strictly prohibited.";
-                        SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-                        smtp.UseDefaultCredentials = false;
-                        smtp.Credentials = new NetworkCredential("ajaybharath009@gmail.com", "ajay009@1234");
-                        smtp.EnableSsl = true;
-                        smtp.Send(mailMsg);
-                        mailMsg.To.Clear();
-                        progressBar1.Value = i + 1;
-                        //label2.Text = $"Dear {dt.Rows[i]["Name"]},You are shortlisted for {dt.Rows[i]["Position"]}, as we discussed your interview was scheduled on {dt.Rows[i]["Date"]} at {dt.Rows[i]["Time"]}";
-                    }
-                    Thread backgroundThread = new Thread(new ThreadStart(() =>
-                    {
-                        for (int i = 0; i < dt.Rows.Count; i++)
+                        //for (int i = 0; i < dt.Rows.Count; i++)
+                        //{
+                        //    //Task.Delay(1000);
+                        //    Thread.Sleep(1000);
+                        //    mailMsg.To.Add(new MailAddress(dt.Rows[i]["Emailid"].ToString()));
+                        //    mailMsg.Subject = "Reg: Ideabytes Interview Call";
+                        //    mailMsg.IsBodyHtml = true;
+                        //    mailMsg.Body = $"Dear {dt.Rows[i]["Name"]},<br/><br/>You are shortlisted for {dt.Rows[i]["Position"]}, as we discussed your interview was scheduled on {dt.Rows[i]["Date"]} at {dt.Rows[i]["Time"]}.<br/><br/>" + @"<img src='https://adminiot.dgtrak.online/FTP_Sensorcnt/domain/IBThanks.png'/>"
+                        //    + "<br/><br/> With Regards <br/> <hr/>HR Manager<br/>Ideabytes Inc<br/>Website: www.ideabytes.com<br/><br/>" + "<img src='https://adminiot.dgtrak.online/FTP_Sensorcnt/domain/IBMailImage.png'/>"
+                        //    + "<br/><br/>Important: This email and any files transmitted with it are confidential and intended solely for the use of the individual or entity to whom they are addressed. If you have received this email in error please notify the system manager. " +
+                        //    "Please notify the sender immediately by e-mail if you have received this e-mail by mistake and delete this e-mail from your system. If you are not the intended recipient you are notified that disclosing, copying, distributing or taking any action in " +
+                        //    "reliance on the contents of this information is strictly prohibited.";
+                        //    richTextBox1.Text += $"\nDear {dt.Rows[i]["Name"]},You are shortlisted for {dt.Rows[i]["Position"]}, as we discussed your interview was scheduled on {dt.Rows[i]["Date"]} at {dt.Rows[i]["Time"]}.";
+                        //    SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                        //    smtp.UseDefaultCredentials = false;
+                        //    smtp.Credentials = new NetworkCredential("ajaybharath009@gmail.com", "ajay009@1234");
+                        //    smtp.EnableSsl = true;
+                        //    smtp.Send(mailMsg);
+                        //    mailMsg.To.Clear();
+                        //    progressBar1.Value = i + 1;
+                        //}
+                        Thread backgroundThread = new Thread(new ThreadStart(() =>
                         {
-                            Thread.Sleep(1000);
-                            this.label2.Invoke(new MethodInvoker(() => {
-                                label2.Text = $"Dear {dt.Rows[i]["Name"]},You are shortlisted for {dt.Rows[i]["Position"]}, as we discussed your interview was scheduled on {dt.Rows[i]["Date"]} at {dt.Rows[i]["Time"]}";
-                            }));
-                        }
-                    }));
-                    backgroundThread.Start();
-                    DialogResult dr1 = MessageBox.Show("Mail Sent Successfully!!", "", MessageBoxButtons.OK);
-                    if (dr1 == DialogResult.OK)
+                            for (int i = 0; i < dt.Rows.Count; i++)
+                            {
+                                Thread.Sleep(1000);
+                                mailMsg.To.Add(new MailAddress(dt.Rows[i]["Emailid"].ToString()));
+                                mailMsg.IsBodyHtml = true;
+                                mailMsg.Body = $"Dear {dt.Rows[i]["Name"]},<br/><br/>You are shortlisted for {dt.Rows[i]["Position"]}, as we discussed your interview was scheduled on {dt.Rows[i]["Date"]} at {dt.Rows[i]["Time"]}.<br/><br/>" + @"<img src='https://adminiot.dgtrak.online/FTP_Sensorcnt/domain/IBThanks.png'/>"
+                                + "<br/><br/> With Regards <br/> <hr/>HR Manager<br/>Ideabytes Inc<br/>Website: www.ideabytes.com<br/><br/>" + "<img src='https://adminiot.dgtrak.online/FTP_Sensorcnt/domain/IBMailImage.png'/>"
+                                + "<br/><br/>Important: This email and any files transmitted with it are confidential and intended solely for the use of the individual or entity to whom they are addressed. If you have received this email in error please notify the system manager. " +
+                                "Please notify the sender immediately by e-mail if you have received this e-mail by mistake and delete this e-mail from your system. If you are not the intended recipient you are notified that disclosing, copying, distributing or taking any action in " +
+                                "reliance on the contents of this information is strictly prohibited.";
+                                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                                smtp.UseDefaultCredentials = false;
+                                smtp.Credentials = new NetworkCredential("ajaybharath009@gmail.com", "ajay009@1234");
+                                smtp.EnableSsl = true;
+                                smtp.Send(mailMsg);
+                                mailMsg.To.Clear();
+                                Invoke(new Action(() =>
+                                {
+                                    richTextBox1.Text += $"\n\nDear {dt.Rows[i]["Name"]},You are shortlisted for {dt.Rows[i]["Position"]},as we discussed\n your interview was scheduled on {dt.Rows[i]["Date"]} at {dt.Rows[i]["Time"]}.";
+                                    progressBar1.Value = i + 1;
+                                }));
+                            }
+                            DialogResult dr1 = MessageBox.Show("Mail Sent Successfully!!", "", MessageBoxButtons.OK);
+                            if (dr1 == DialogResult.OK)
+                            {
+                                Invoke(new Action(() =>
+                                {
+                                    progressBar1.Value = 0;
+                                    label2.Text = "";
+                                    label1.Text = "";
+                                    progressBar1.Visible = false;
+                                }));
+                            }
+                        }));
+                        backgroundThread.Start();
+                    }
+                    else
                     {
-                        progressBar1.Value = 0;
-                        label2.Text = "";
+                        MessageBox.Show("Mails sending Cancelled!!!");
                         label1.Text = "";
                     }
                 }
                 else
                 {
-                    MessageBox.Show("Mails sending Cancelled!!!"); 
-                    label1.Text = "";
+                    MessageBox.Show("Please Upload Excel File");
                 }
+                button1.Enabled = true;
+                button2.Enabled = true;
+
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Please Upload Excel File");
+                MessageBox.Show("The Mails sending was not Completed!!!!");
             }
-            button1.Enabled = true;
-            button2.Enabled = true;
         }
         private void Form1_Load(object sender, EventArgs e)
         {
             //dataGridView1.Visible = true;
+            progressBar1.Visible = false;
         }
     }
 }
